33dfd5fdfbc31ee25f22ec9efcd056ddd2065d76
MitchelMA <vonkm124@gmail.com>
2023-08-02 17:05:57 +0200

    extracting nummers uit console input

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "string/stringbuilder.h"
 #include "string/stringview.h"
+
+void num_example(void);
+NODISCARD
+vector_t text_retrieve_numbers(const string_t* text);
 
 int main(void)
 {
@@ -52,5 +57,75 @@ int main(void)
 
     string_clean(&str1);
     stringbuilder_clean(&str_builder);
+    puts("");
+    num_example();
     return EXIT_SUCCESS;
+}
+
+void num_example(void)
+{
+    stringbuilder_t builder;
+    string_t text;
+    vector_t numbers = NULL;
+
+    builder  = stringbuilder_create();
+    printf("Text with numbers throughout: ");
+    stringbuilder_read_console(&builder);
+    char removed = stringbuilder_dequeue(&builder);
+    if(removed != '\n') stringbuilder_append_ch(&builder, removed);
+
+    text = stringbuilder_build(&builder);
+    stringbuilder_clean(&builder);
+
+    numbers = text_retrieve_numbers(&text);
+    string_clean(&text);
+    puts("Numbers found: ");
+    for(size_t i = 0; i < vector_get_elem_count(numbers); i++)
+    {
+        long long* number;
+        vector_elem_at(numbers, i, (void**) &number);
+        printf("\t%lld\n", *number);
+    }
+
+    vector_clean(numbers);
+}
+
+vector_t text_retrieve_numbers(const string_t* text)
+{
+    bool found_number = false;
+    stringview_t str_view = stringview_create(text, 0, 0);
+    str_view.count = 0;
+    vector_t num_vector = vector_create(sizeof(long long));
+    long long number = 0;
+
+    while((str_view.start_idx + str_view.count) < str_view.str_->count_)
+    {
+        char at_current_pos = str_view.str_->text_[str_view.start_idx + str_view.count];
+        bool prev_value = found_number;
+
+        found_number = ((at_current_pos == '-' || at_current_pos == '+') && str_view.count == 0) || isdigit(at_current_pos);
+        if(prev_value == true && found_number == false)
+        {
+            number = stringview_atoll(&str_view);
+            vector_append(num_vector, (void*) &number);
+        }
+
+        if(found_number)
+        {
+            str_view.count++;
+        }
+        else
+        {
+            str_view.start_idx += str_view.count == 0 ? 1 : str_view.count+1;
+            str_view.count = 0;
+        }
+    }
+
+    if(str_view.count > 0)
+    {
+        number = stringview_atoll(&str_view);
+        vector_append(num_vector, (void*) &number);
+    }
+
+    return num_vector;
 }
