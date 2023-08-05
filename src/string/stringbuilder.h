99f33e317e9b6b5077683e0030bcc1fc7441a8d8
MitchelMA <32548@ma-web.nl>
2023-08-05 15:49:44 +0200

    file-reading has been tested on both windows and linux successfully now

--- src/string/stringbuilder.h
+++ src/string/stringbuilder.h
@@ -56,8 +56,6 @@ void stringbuilder_ltoa(stringbuilder_t* str_builder, long value);
 void stringbuilder_lltoa(stringbuilder_t* str_builder, long long value);
 void stringbuilder_stoa(stringbuilder_t* str_builder, size_t value);
 
-// Todo! Testing of the stringbuilder on windows and with files instead of only the stdin
-
 bool stringbuilder_read(FILE* fstream, stringbuilder_t* str_builder, size_t max_read_count);
 #define stringbuilder_read_console(STR_BUILDER_PTR) stringbuilder_read(stdin, (STR_BUILDER_PTR), ~(size_t) 0)
 
